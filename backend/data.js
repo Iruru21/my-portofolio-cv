ea95c9fbeee0c863f025d96974463222d009beb8
Iruru21 <khaifikk121@gmail.com>
2025-07-10 12:57:08 +0700

    website portofolio

---
+++ backend/data.js
@@ -0,0 +1,56 @@
+// backend/data.js
+export const educationHistory = [
+  {
+    institution: 'STM Pembangunan Yogyakarta',
+    degree: 'Kimia Industri',
+    period: '2019 - 2023',
+    description: '',
+  },
+
+   {
+    institution: 'Universitas Amikom',
+    degree: 'S1 Teknik Informatika',
+    period: '2023 - ',
+    description: 'Fokus belajar sampai bisa.',
+  },
+]
+
+export const skills = [
+  { id : '1', name: 'Public Speaking', level: 'Skill otodidak yang saya asah sendiri demi keberlangsungan saya kedepannya', url : 'src/assets/PublicSpeaking.png' },
+  {id : '2', name: 'Singing', level: 'Advanced', url :'src/assets/kitaaa.png' },
+  { id : '3', name: 'Wotagei', level: 'Intermediate', url :'src/assets/kitaaa.png' },
+]
+
+
+export const projects = [
+  {
+    title: 'Portfolio Website',
+    description: 'Website pribadi untuk menampilkan profil dan proyek.',
+    techStack: ['Vue.js', 'Tailwind CSS'],
+    link: 'https://your-portfolio-site.com',
+    github: 'https://github.com/Iruru21',
+  },
+
+  {
+    title: 'Project Wotagei',
+    description: 'Berisikan kegiatan wotagei dalam berupa project.',
+    techStack: ['Vue.js', 'Tailwind CSS'],
+    link: 'https://your-portfolio-site.com',
+    github: 'https://github.com/Iruru21',
+  },
+  {
+    title: 'Project Penanaman Pipa Gas Induk',
+    description: 'Project IKN pemasangan pipa gas induk yang dilaksanakan di Kalimantan Timur.',
+    techStack: ['Vue.js', 'Tailwind CSS'],
+    link: 'https://your-portfolio-site.com',
+    github: 'https://github.com/Iruru21',
+  },
+
+   {
+    title: 'Praktek Kerja Lapangan',
+    description: 'PKL dilaksanakan di PT. Coconut Center yang berlokasi di Kota Jogjakarta.',
+    techStack: ['Vue.js', 'Tailwind CSS'],
+    link: 'https://your-portfolio-site.com',
+    github: 'https://github.com/Iruru21',
+  }
+]
